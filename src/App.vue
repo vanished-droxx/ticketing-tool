5b4c6f67044bacdbfac4b3b81b38a69bd22c9788
vanished <nikurb@icloud.com>
2022-09-19 20:05:42 +0200

    changes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>hello</h1>
+  <p>{{collections}}</p>
+</template>
+
+<script setup>
+import PocketBase from 'pocketbase';
+
+const client = new PocketBase('http://127.0.0.1:8090');
+
+client.admins.authViaEmail('nikurb@icloud.com', '1234567890');
+
+const collections = client.collections.getFullList(200 /* batch size */, { sort: '-created' });
+
+console.log(collections)
+
+
+</script>
+
+<style>
+
+</style>
